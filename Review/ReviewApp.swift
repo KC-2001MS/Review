4d6471b4ffbf369e1220e257d6a275de9b1f2147
KC-2001MS <iroiro.work1234@gmail.com>
2023-02-03 15:23:54 +0900

    Initial Commit

---
+++ Review/ReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReviewApp.swift
+//  Review
+//
+//  Created by 茅根啓介 on 2023/02/03.
+//
+
+import SwiftUI
+
+@main
+struct ReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
